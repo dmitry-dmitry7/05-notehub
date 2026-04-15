1f1e1192b4c59b837c7204cbe2ff4a8de29e610c
Dmytro <nova_dima@ukr.net>
2026-04-16 01:48:58 +0300

    add components structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,13 @@
-import './App.css';
+import css from './App.module.css';
 
 export default function App() {
-  return <></>;
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        {/* Компонент SearchBox */}
+        {/* Пагінація */}
+        {/* Кнопка створення нотатки */}
+      </header>
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/App.tsx';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import App from './components/App/App';
 // Нормалізація стилів
 import 'modern-normalize';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
